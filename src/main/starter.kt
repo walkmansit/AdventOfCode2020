c02a48c29b9551ac2b5df33d1c84907c1013d2cd
AndreyNagaev <walkmansit@gmail.com>
2020-12-03 12:10:36 +0400

    day3

--- src/main/starter.kt
+++ src/main/starter.kt
@@ -2,11 +2,16 @@ package main
 
 import main.days.Day1
 import main.days.Day2
+import main.days.Day3
 
 fun main(args : Array<String>) {
     //println(Day1.getResult())
     //println(Day1.getResultAdvanced())
 
-    println(Day2.getResult())
-    println(Day2.getResultAdvanced())
+    //println(Day2.getResult())
+    //println(Day2.getResultAdvanced())
+
+    println(Day3.getResult())
+    println(Day3.getResultAdvanced())
 }
+
